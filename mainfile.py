a42cb27efa3d4aa50ff802b801cef676e46b3b88
Blaze <68640945+thebeastof@users.noreply.github.com>
2023-04-05 09:16:27 +0300

    finished program

---
+++ mainfile.py
@@ -0,0 +1,359 @@
+def main():
+    legion5gen7 = {
+        "name": "Legion 5 Gen 7 (15” AMD) Gaming Laptop",
+        "CPU Intensive": True,
+        "RAM Intensive": True,
+        "GPU Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    legion5igen7 = {
+        "name": "Legion 5i Gen 7 (15” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    legion5progen7 = {
+        "name": "Legion 5 Pro Gen 7 (16” AMD) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    legion5iprogen7 = {
+        "name": "Legion 5i Pro Gen 7 (16” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    legion5gen6 = {
+        "name": "Legion 5 Gen 6 (17” AMD) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    legionslim7gen7 = {
+        "name": "Legion Slim 7 Gen 7 (16” AMD) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    legionslim7igen7 = {
+        "name": "Legion Slim 7i Gen 7 (16” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    legion7gen7 = {
+        "name": "Legion Slim 7 Gen 7 (16” AMD) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    legion7igen7 = {
+        "name": "Legion 7i Gen 7 (16” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    legionpro5igen8 = {
+        "name": "Legion Pro 5i Gen 8 (16” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    legionpro7igen8 = {
+        "name": "Legion Pro 7i Gen 8 (16” Intel) Gaming Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": True,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    thinkpadx1carbongen10 = {
+        "name": "ThinkPad X1 Carbon Gen 10 (14” Intel) Laptop",
+        "CPU Intensive": False,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    thinkpadx1nanogen2 = {
+        "name": "ThinkPad X1 Nano Gen 2 (13” Intel) Laptop",
+        "CPU Intensive": False,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    thinkpadz13 = {
+        "name": "ThinkPad Z13 (13” AMD) Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    thinkpadz16 = {
+        "name": "ThinkPad Z16 (16” AMD) Laptop",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom1max = {
+        "name": "Macbook Pro M1 Max",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom1promax = {
+        "name": "Macbook Pro M1 Pro Max",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom2max = {
+        "name": "Macbook Pro M2 Max",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom2promax = {
+        "name": "Macbook Pro M2 Pro Max",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom1promax = {
+        "name": "Macbook Pro M1 Pro Max",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom1 = {
+        "name": "Macbook Pro M1",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookprom2 = {
+        "name": "Macbook Pro M2",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    macbookairm1 = {
+        "name": "Macbook Air M1",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    macbookairm2 = {
+        "name": "Macbook Air M2",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": False,
+        "High-Cost": True
+    }
+
+    aspirevero = {
+        "name": "Aspire Vero",
+        "CPU Intensive": False,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    aspire3intel = {
+        "name": "Aspire 3 Intel",
+        "CPU Intensive": False,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    swift5 = {
+        "name": "Acer Swift 5",
+        "CPU Intensive": False,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+
+    swift3amd = {
+        "name": "Acer Swift 3 AMD",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    swift3intel = {
+        "name": "Acer Swift 3 Intel",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": False,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    swiftgo = {
+        "name": "Acer Swift Go",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": True,
+        "Average": False,
+        "High-Cost": False
+    }
+
+    swiftx14 = {
+        "name": "Acer Swift X14",
+        "CPU Intensive": True,
+        "GPU Intensive": False,
+        "RAM Intensive": True,
+        "Low-Cost": False,
+        "Average": True,
+        "High-Cost": False
+    }
+    laptops = [swiftx14, swiftgo, swift3intel, swift3amd, swift5, aspire3intel, aspirevero,
+               macbookairm1, macbookairm2, macbookprom1, macbookprom1promax, macbookprom1max, macbookprom2,
+               macbookprom2max, macbookprom2promax, thinkpadx1carbongen10, thinkpadz16, thinkpadx1nanogen2,
+               thinkpadz13, legion5gen6, legion5gen7, legion5igen7, legion5progen7, legionslim7igen7, legionslim7gen7,
+               legionpro5igen8, legion7gen7, legionpro7igen8, legion7igen7, legion5iprogen7]
+    print("Disclaimer: Use the prompts in the parantheses to answer the questions otherwise the program will not work")
+    b = str(input("What is your budget (Low-Cost($0 - $1099), Average($1100 - $1499), or High-Cost($1500 +))?: "))
+    g = str(input("What are you going to use this laptop for?(General Use(School or work), Gaming, Streaming, Content Creating(photo editing, video editing, art), Program Development): "))
+    r = str(input("Are you going to be using graphics intensive programs(y/n): "))
+    m = str(input("Are you going to be multitasking or using multiple apps and programs at the same time(y/n)?: "))
+    output = []
+    for i in laptops: 
+        if g == "Gaming" or "Streaming" or "Content Creating":
+            if r == "y":
+                if i["GPU Intensive"] == True:
+                        if i["CPU Intensive"] and i["RAM Intensive"] == True:
+                            if b == "Low-Cost":
+                                if i["Low-Cost"] == True:
+                                    output.append(i["name"])
+                            if b == "Average":
+                                if i["Average"] == True:
+                                    output.append(i["name"])
+                            if b == "High-Cost":
+                                if i["High-Cost"] == True:
+                                    output.append(i["name"])
+
+        if g == "General Use" and b == "Low-Cost":
+            if i["Low-Cost"] == True:
+                if i["GPU Intensive"] == False:
+                    output.append(i["name"])
+        if g == "General Use" and b == "High-Cost":
+            if i["High-Cost"] == True:
+                if i["GPU Intensive"] == False:
+                    output.append(i["name"])
+        elif g == "General Use" and (r or m == "n"):
+            print("Such a computer does not exist")
+
+        if g == "Program Development" or m == "y":
+            if r == "n":
+                if i["CPU Intensive"] and i["RAM Intensive"] == True:
+                    if i["GPU Intensive"] == False:
+                        output.append(i["name"])
+
+    print(output)  
+   
+if __name__ == "__main__":
+    main() 
+                
+
